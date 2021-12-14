490c00ad2ba4f67dba36c2e001aa7256555f1e39
MinisamZ <minisam2468@gmail.com>
2021-12-14 19:19:16 +0600

    Test page

--- src/main/java/project/view/MainPageServlet.java
+++ src/main/java/project/view/MainPageServlet.java
@@ -19,20 +19,258 @@ public class MainPageServlet extends HttpServlet {
     public void generateView(HttpServletRequest request,
                              HttpServletResponse response)
             throws IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
-        out.println("<html lang=\"en\"><head>\n" +
+        out.println("\n" +
+                "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
                 "  <meta charset=\"UTF-8\">\n" +
                 "  <title>Блог онлайн</title>\n" +
                 "\n" +
-                "  <!-- Bootstrap Grid -->\n" +
-                "  <link rel=\"stylesheet\" type=\"text/css\" href=\"/media/assets/bootstrap-grid-only/css/grid12.css\">\n" +
+                "  <!-- Bootstrap CSS (jsDelivr CDN) -->\n" +
+                "  <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">\n" +
+                "  <!-- Bootstrap Bundle JS (jsDelivr CDN) -->\n" +
+                "  <script defer src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p\" crossorigin=\"anonymous\"></script>\n" +
                 "\n" +
                 "  <!-- Google Fonts -->\n" +
                 "  <link href=\"https://fonts.googleapis.com/css?family=Roboto:300,400,500,700\" rel=\"stylesheet\">\n" +
                 "\n" +
                 "  <!-- Custom -->\n" +
-                "  <link rel=\"stylesheet\" type=\"text/css\" href=\"/media/css/style.css\">\n" +
+                "  <style type=\"text/css\">\n" +
+                "  html, body, div, span, applet, object, iframe,\n" +
+                "h1, h2, h3, h4, h5, h6, p, blockquote, pre,\n" +
+                "a, abbr, acronym, address, big, cite, code,\n" +
+                "del, dfn, em, img, ins, kbd, q, s, samp,\n" +
+                "small, strike, strong, sub, sup, tt, var,\n" +
+                "b, u, i, center,\n" +
+                "dl, dt, dd, ol, ul, li,\n" +
+                "fieldset, form, label, legend,\n" +
+                "table, caption, tbody, tfoot, thead, tr, th, td,\n" +
+                "article, aside, canvas, details, embed,\n" +
+                "figure, figcaption, footer, header, hgroup,\n" +
+                "menu, nav, output, ruby, section, summary,\n" +
+                "time, mark, audio, video {\n" +
+                "  margin: 0;\n" +
+                "  padding: 0;\n" +
+                "  border: 0;\n" +
+                "  font-size: 100%;\n" +
+                "  font: inherit;\n" +
+                "  vertical-align: baseline; }\n" +
+                "\n" +
+                "/* HTML5 display-role reset for older browsers */\n" +
+                "article, aside, details, figcaption, figure,\n" +
+                "footer, header, hgroup, menu, nav, section {\n" +
+                "  display: block; }\n" +
+                "\n" +
+                "body {\n" +
+                "  line-height: 1; }\n" +
+                "\n" +
+                "ol, ul {\n" +
+                "  list-style: none; }\n" +
+                "\n" +
+                "blockquote, q {\n" +
+                "  quotes: none; }\n" +
+                "\n" +
+                "blockquote:before, blockquote:after,\n" +
+                "q:before, q:after {\n" +
+                "  content: '';\n" +
+                "  content: none; }\n" +
+                "\n" +
+                "table {\n" +
+                "  border-collapse: collapse;\n" +
+                "  border-spacing: 0; }\n" +
+                "\n" +
+                "h1 {\n" +
+                "  font-size: 48px;\n" +
+                "  font-weight: bold; }\n" +
+                "\n" +
+                "h2 {\n" +
+                "  font-size: 34px;\n" +
+                "  font-weight: bold; }\n" +
+                "\n" +
+                "h3 {\n" +
+                "  font-size: 20px;\n" +
+                "  font-weight: bold; }\n" +
+                "\n" +
+                "h4 {\n" +
+                "  font-size: 16px;\n" +
+                "  font-weight: bold; }\n" +
+                "\n" +
+                "h5 {\n" +
+                "  font-size: 14px;\n" +
+                "  font-weight: bold; }\n" +
+                "\n" +
+                "h6 {\n" +
+                "  font-size: 12px;\n" +
+                "  font-weight: bold; }\n" +
+                "\n" +
+                "body {\n" +
+                "  font-family: 'Roboto', sans-serif; }\n" +
+                "\n" +
+                "#header {\n" +
+                "  margin-bottom: 25px; }\n" +
+                "  #header .header__top {\n" +
+                "    padding: 37px 0; }\n" +
+                "    #header .header__top .header__top__logo {\n" +
+                "      float: left; }\n" +
+                "    #header .header__top .header__top__menu {\n" +
+                "      padding-top: 15px;\n" +
+                "      text-align: right; }\n" +
+                "      #header .header__top .header__top__menu ul > li {\n" +
+                "        display: inline-block;\n" +
+                "        margin-right: 35px; }\n" +
+                "        #header .header__top .header__top__menu ul > li > a {\n" +
+                "          font-size: 20px;\n" +
+                "          text-decoration: none;\n" +
+                "          color: black; }\n" +
+                "          #header .header__top .header__top__menu ul > li > a:hover {\n" +
+                "            border-bottom: 2px solid #333; }\n" +
+                "  #header .header__bottom {\n" +
+                "    height: 50px;\n" +
+                "    background-color: black;\n" +
+                "    line-height: 50px; }\n" +
+                "    #header .header__bottom nav ul > li {\n" +
+                "      display: inline-block;\n" +
+                "      margin-right: 34px; }\n" +
+                "      #header .header__bottom nav ul > li > a {\n" +
+                "        color: white;\n" +
+                "        font-size: 20px;\n" +
+                "        text-decoration: none;\n" +
+                "        padding: 3px 10px; }\n" +
+                "        #header .header__bottom nav ul > li > a:hover {\n" +
+                "          border-top: 2px solid white;\n" +
+                "          border-bottom: 2px solid white; }\n" +
+                "\n" +
+                ".block {\n" +
+                "  background-color: #ebebeb;\n" +
+                "  padding: 36px 42px;\n" +
+                "  margin-bottom: 25px; }\n" +
+                "  .block > h3 {\n" +
+                "    font-weight: normal; }\n" +
+                "  .block > a {\n" +
+                "    float: right;\n" +
+                "    font-size: 20px;\n" +
+                "    color: black;\n" +
+                "    text-decoration: none;\n" +
+                "    font-weight: 300; }\n" +
+                "    .block > a:hover {\n" +
+                "      border-bottom: 1px solid #333; }\n" +
+                "  .block .block__content {\n" +
+                "    padding-top: 40px; }\n" +
+                "\n" +
+                ".articles.articles__horizontal {\n" +
+                "  font-size: 0;\n" +
+                "  margin: -12px; }\n" +
+                "  .articles.articles__horizontal .article {\n" +
+                "    margin-bottom: 22px;\n" +
+                "    display: inline-block;\n" +
+                "    vertical-align: top;\n" +
+                "    width: 50%;\n" +
+                "    font-size: 16px;\n" +
+                "    padding: 12px; }\n" +
+                "\n" +
+                ".articles.articles__vertical .article {\n" +
+                "  margin-bottom: 22px; }\n" +
+                "\n" +
+                ".articles .article:last-child {\n" +
+                "  margin-bottom: 0; }\n" +
+                "\n" +
+                ".articles .article:after {\n" +
+                "  visibility: hidden;\n" +
+                "  display: block;\n" +
+                "  font-size: 0;\n" +
+                "  content: \" \";\n" +
+                "  clear: both;\n" +
+                "  height: 0; }\n" +
+                "\n" +
+                ".articles .article .article__image {\n" +
+                "  width: 125px;\n" +
+                "  height: 125px;\n" +
+                "  overflow: hidden;\n" +
+                "  background-color: black;\n" +
+                "  float: left;\n" +
+                "  background-repeat: no-repeat;\n" +
+                "  background-size: cover;\n" +
+                "  background-position: center center; }\n" +
+                "\n" +
+                ".articles .article .article__info {\n" +
+                "  margin-left: 125px;\n" +
+                "  padding-left: 21px; }\n" +
+                "  .articles .article .article__info > a {\n" +
+                "    color: black;\n" +
+                "    font-size: 18px;\n" +
+                "    font-weight: normal;\n" +
+                "    text-decoration: none; }\n" +
+                "    .articles .article .article__info > a:hover {\n" +
+                "      border-bottom: 2px solid #333; }\n" +
+                "  .articles .article .article__info > .article__info__meta {\n" +
+                "    font-size: 14px;\n" +
+                "    font-weight: 300;\n" +
+                "    margin-top: 9px;\n" +
+                "    margin-bottom: 13px; }\n" +
+                "    .articles .article .article__info > .article__info__meta a {\n" +
+                "      color: black;\n" +
+                "      text-decoration: none; }\n" +
+                "      .articles .article .article__info > .article__info__meta a:hover {\n" +
+                "        border-bottom: 1px solid #333; }\n" +
+                "  .articles .article .article__info > .article__info__preview {\n" +
+                "    font-size: 16px;\n" +
+                "    font-weight: 300; }\n" +
+                "\n" +
+                "#footer {\n" +
+                "  background-color: #ebebeb;\n" +
+                "  padding: 37px 0; }\n" +
+                "  #footer .footer__logo {\n" +
+                "    float: left; }\n" +
+                "  #footer .footer__menu {\n" +
+                "    padding-top: 15px;\n" +
+                "    text-align: right; }\n" +
+                "    #footer .footer__menu ul > li {\n" +
+                "      display: inline-block;\n" +
+                "      margin-right: 35px; }\n" +
+                "      #footer .footer__menu ul > li > a {\n" +
+                "        font-size: 20px;\n" +
+                "        text-decoration: none;\n" +
+                "        color: black;\n" +
+                "        font-weight: 300; }\n" +
+                "        #footer .footer__menu ul > li > a:hover {\n" +
+                "          border-bottom: 1px solid #333; }\n" +
+                "\n" +
+                ".full-text {\n" +
+                "  font-size: 18px;\n" +
+                "  color: #3a3a3a;\n" +
+                "  line-height: 1.7em; }\n" +
+                "  .full-text p {\n" +
+                "    text-indent: 0;\n" +
+                "    margin: 0 0 26px 0; }\n" +
+                "  .full-text h1, .full-text h2, .full-text h3, .full-text h4, .full-text h5, .full-text h6 {\n" +
+                "    margin: 52px 0 26px 0; }\n" +
+                "\n" +
+                ".form .form__group {\n" +
+                "  margin-bottom: 20px; }\n" +
+                "  .form .form__group:last-child {\n" +
+                "    margin-bottom: 0; }\n" +
+                "\n" +
+                ".form .form__control {\n" +
+                "  width: 100%;\n" +
+                "  padding: 10px;\n" +
+                "  font-size: 16px;\n" +
+                "  outline: none; }\n" +
+                "\n" +
+                ".form textarea.form__control {\n" +
+                "  min-height: 100px; }\n" +
+                "\n" +
+                ".form input[type=\"submit\"].form__control {\n" +
+                "  background-color: black;\n" +
+                "  color: white;\n" +
+                "  border: 3px solid white;\n" +
+                "  width: 250px;\n" +
+                "  opacity: 0.7; }\n" +
+                "  .form input[type=\"submit\"].form__control:hover {\n" +
+                "    cursor: pointer;\n" +
+                "    opacity: 1; }\n" +
+                "  </style>\n" +
                 "</head>\n" +
                 "<body>\n" +
                 "\n" +
@@ -249,7 +487,7 @@ public class MainPageServlet extends HttpServlet {
                 "            <div class=\"block\">\n" +
                 "  <h3>Мы_знаем</h3>\n" +
                 "  <div class=\"block__content\">\n" +
-                "    <iframe style=\"background:transparent !important\" scrolling=\"no\" frameborder=\"0\" allowtransparency=\"true\" width=\"320\" height=\"320\" src=\"//ra.revolvermaps.com/w/6/a/c2.php?i=02op3nb0crr&amp;m=7&amp;s=320&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80\"></iframe><script type=\"text/javascript\" src=\"//ra.revolvermaps.com/0/0/6.js?i=02op3nb0crr&amp;m=7&amp;s=320&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80\" async=\"async\"></script>\n" +
+                "    <iframe style=\"background:transparent !important\" scrolling=\"no\" frameborder=\"0\" allowtransparency=\"true\" width=\"320\" height=\"320\" src=\"//ra.revolvermaps.com/w/6/a/c2.php?i=02op3nb0crr&amp;m=7&amp;s=320&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80\"></iframe>\n" +
                 "  </div>\n" +
                 "</div>\n" +
                 "\n" +
@@ -389,8 +627,8 @@ public class MainPageServlet extends HttpServlet {
                 "  </div>\n" +
                 "</footer>  </div>\n" +
                 "\n" +
-                "\n" +
-                "</body></html>");
+                "</body>\n" +
+                "</html>");
 
     }
 }
--- src/main/java/project/view/TestFormServlet.java
+++ src/main/java/project/view/TestFormServlet.java
@@ -3,8 +3,7 @@ package project.view;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 
 public class TestFormServlet extends HttpServlet {
@@ -23,8 +22,18 @@ public class TestFormServlet extends HttpServlet {
         // Set page title
 
         // Specify the content type is HTML
-        response.setContentType("text/html");
+        response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
+
+
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in, "Cp866"));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out, "Cp866"), true);
+
+        System.out.println("out + " + out.toString());
+        System.out.println("br + " + br.toString());
+        System.out.println("pw + " + pw.toString());
+
         try {
             out.println("Привет");
         } finally {
